1104d926fe4d1de98f25660970c170aa74c1ff64
Vitaliy Murzin <murzinvit@yandex.ru>
2024-07-03 01:15:44 +0500

    step 1

--- src/main/java/ru/qimix/Main.java
+++ src/main/java/ru/qimix/Main.java
@@ -5,16 +5,24 @@ import java.util.*;
 public class Main {
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
 
-    public static void main(String[] args) {
-        System.out.println(generateRoute("RLRFR", 100));
+
+    public static void main(String[] args) throws InterruptedException {
+        for(int i = 0; i < 1000; i++) {
+            System.out.println(generateRoute("RLRFR", 100));
+        }
     }
 
-    public static String generateRoute(String letters, int length) {
+    public static String generateRoute (String letters,int length) throws InterruptedException {
         Random random = new Random();
         StringBuilder route = new StringBuilder();
-        for (int i = 0; i < length; i++) {
-            route.append(letters.charAt(random.nextInt(letters.length())));
-        }
+        Runnable task = () -> {
+            for (int i = 0; i < length; i++) {
+                route.append(letters.charAt(random.nextInt(letters.length())));
+            }
+        };
+        Thread thread = new Thread(task);
+        thread.start();
+        thread.join();
         return route.toString();
     }
 
